83f2b307720fbbdc29959c6b9a2847c782f59eb9
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-12-12 13:18:04 +0530

    Refactor `PSReadLineKeyHandler`

--- NLPowerShell.psm1
+++ NLPowerShell.psm1
@@ -14,40 +14,44 @@ if (-Not (Test-Path -Path $Script:CONFIG_PATH)) { return }
 $Script:CONFIG = Import-Clixml -Path $Script:CONFIG_PATH
     
 # Register the PSReadLineKeyHandler
-Set-PSReadLineKeyHandler -Key "Ctrl+Shift+RightArrow" `
-    -BriefDescription "Natural Language to PowerShell" `
-    -LongDescription "Convert Natural Language to PowerShell using OpenAI" `
-    -ScriptBlock {
+$PSReadLineKeyHandlerOptions = @{
+    Key              = "Ctrl+Shift+RightArrow"
+    BriefDescription = "Use Natural Language to interact with PowerShell"
+}
+Set-PSReadLineKeyHandler @PSReadLineKeyHandlerOptions -ScriptBlock {
     param ($Key, $Arg)
 
+    # Instantiate $Line and $Cursor
     $Line = $null
     $Cursor = $null
-
     [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$Line, [ref]$Cursor)
 
+    # If the line is a comment ...
     if ($Line.StartsWith("#")) {
-        # get response from the Get-NLPowerShellCommand function
-        $Output = Get-NLPowerShellCommand -Line $Line
-        
-        # exit if the output is null
-        if ($null -eq $Output) { return }
+        # ... get response from the Get-NLPowerShellCommand function
+        $SuggestedCommand = Get-NLPowerShellCommand -Line $Line
+        if ($null -eq $SuggestedCommand) { return }
     
+        # Delete the entire line
         [Microsoft.PowerShell.PSConsoleReadLine]::BackwardDeleteLine()
-        foreach ($Str in $Output) {
+        foreach ($Str in $SuggestedCommand) {
             if ($null -ne $Str -and $Str -ne "") {
+                # Append the generated command
                 [Microsoft.PowerShell.PSConsoleReadLine]::Insert($Str)
             }
         }
+        # Readd the original comment
         [Microsoft.PowerShell.PSConsoleReadLine]::Insert("    $Line")
     }
+
+    # If the line is not a command and an explanation is required ...
     else {
-        # get response from the Get-NLPowerShellExplanation function
-        $Output = Get-NLPowerShellExplanation -Line $Line
+        # ... get response from the Get-NLPowerShellExplanation function
+        $SuggestedExplanation = Get-NLPowerShellExplanation -Line $Line
+        if ($null -eq $SuggestedExplanation) { return }
 
-        # exit if the output is null
-        if ($null -eq $Output) { return }
-
-        foreach ($Str in $Output) {
+        # Append an explanation at the end as a comment
+        foreach ($Str in $SuggestedExplanation) {
             if ($null -ne $Str -and $Str -ne "") {
                 [Microsoft.PowerShell.PSConsoleReadLine]::Insert("    # $Str")
             }
