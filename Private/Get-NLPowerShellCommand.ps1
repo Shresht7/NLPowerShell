858f51b14d0b562015bd0c6087cd900111169ef0
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-12-12 13:08:07 +0530

    Update docstrings

--- Private/Get-NLPowerShellCommand.ps1
+++ Private/Get-NLPowerShellCommand.ps1
@@ -4,17 +4,12 @@
 .DESCRIPTION
     The Get-CommandCompletion function takes a natural language prompt as input
     and uses the OpenAI API to generate a corresponding PowerShell command
-.PARAMETER Comment
-    The natural language prompt to convert to a PowerShell command.
-    This parameter is mandatory and should not be empty or null.
-.OUTPUTS
-    System.String. The generated PowerShell command.
 .NOTES
     This function uses the OpenAI API to generate the PowerShell command.
     You must provide a valid API key and model name in the CONFIG script variable to use this function.
 #>
 function Get-NLPowerShellCommand(
-    # The comment to parse
+    # The natural language prompt to convert to a PowerShell command
     [Parameter(Mandatory)]
     [ValidateNotNullOrEmpty()]
     [Alias("Line")]
@@ -27,7 +22,7 @@ function Get-NLPowerShellCommand(
     $Prompt = "<# PowerShell #>`n# Write a PowerShell command to do the following:`n$Comment`n"
 
     # Invoke the OpenAI Completions API
-   $Response = Invoke-OpenAICompletion -Prompt $Prompt
+    $Response = Invoke-OpenAICompletion -Prompt $Prompt
 
     # Get the best-fit result and process it for output
     $Result = $Response.choices[0].text.Trim()
