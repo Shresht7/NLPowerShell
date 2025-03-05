7f44e84da9737eb431b5dc276696ff472c6455d0
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-03-05 16:41:11 +0530

    Switch to a more accurate model

--- NLPowerShell.psm1
+++ NLPowerShell.psm1
@@ -3,4 +3,4 @@ Get-ChildItem -Path $PSScriptRoot -Recurse -Filter "*.ps1" | ForEach-Object {
     . $_.FullName -Force
 }
 
-Initialize-NLPowerShell -Ollama -Model "llama3.2"
+Initialize-NLPowerShell -Ollama -Model "qwen2.5-coder"
