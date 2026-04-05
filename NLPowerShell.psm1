8d1dfc9f68c62e6086e3ded7ca6880a8258bad10
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-04-05 12:11:25 +0530

    Refactor script imports to explicitly load classes, private helpers, and public functions

--- NLPowerShell.psm1
+++ NLPowerShell.psm1
@@ -1,4 +1,14 @@
-# Import PowerShell Scripts
-Get-ChildItem -Path $PSScriptRoot -Recurse -Filter "*.ps1" | ForEach-Object {
+# Explicitly import Classes
+Get-ChildItem -File -Path (Join-Path $PSScriptRoot "Class") -Filter "*.ps1" | ForEach-Object {
+    . $_.FullName -Force
+}
+
+# Import Private Helpers
+Get-ChildItem -File -Path (Join-Path $PSScriptRoot "Private") -Filter "*.ps1" -Recurse | ForEach-Object {
+    . $_.FullName -Force
+}
+
+# Import Public Functions
+Get-ChildItem -File -Path (Join-Path $PSScriptRoot "Public") -Filter "*.ps1" -Recurse | ForEach-Object {
     . $_.FullName -Force
 }
